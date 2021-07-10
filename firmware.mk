813f394539a429f604b6352b000b9c17e0127799
Qisthi Iskandar Haqiki <qisthih@gmail.com>
2021-07-10 14:04:34 +0000

    markw : Initial Firmware from MIUI V10.2.2.0.MBECNXM

---
+++ firmware.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/firmware/firmware-update/adspso.bin:install/firmware-update/adspso.bin \
+    vendor/xiaomi/firmware/firmware-update/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi/firmware/firmware-update/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi/firmware/firmware-update/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi/firmware/firmware-update/emmc_appsboot.mbn:install/firmware-update/emmc_appsboot.mbn \
+    vendor/xiaomi/firmware/firmware-update/keymaster.mbn:install/firmware-update/keymaster.mbn \
+    vendor/xiaomi/firmware/firmware-update/lksecapp.mbn:install/firmware-update/lksecapp.mbn \
+    vendor/xiaomi/firmware/firmware-update/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi/firmware/firmware-update/rpm.mbn:install/firmware-update/rpm.mbn \
+    vendor/xiaomi/firmware/firmware-update/sbl1.mbn:install/firmware-update/sbl1.mbn \
+    vendor/xiaomi/firmware/firmware-update/splash.img:install/firmware-update/splash.img \
+    vendor/xiaomi/firmware/firmware-update/tz.mbn:install/firmware-update/tz.mbn 
